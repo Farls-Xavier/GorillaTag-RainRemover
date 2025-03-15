dcd3fb0363cecebc71fc3d09a46d8309f3176f67
The coolest farley <surelyilikemen@gmail.com>
2025-03-15 16:23:51 -0500

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,88 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace RainRemover
+{
+    public static class BuildInfo
+    {
+        public const string Name = "RainRemover"; // Name of the Mod.  (MUST BE SET)
+        public const string Description = "Removes rain from gorilla tag!"; // Description for the Mod.  (Set as null if none)
+        public const string Author = "Farleyy"; // Author of the Mod.  (MUST BE SET)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class RainRemover : MelonMod
+    {
+        public static void RemoveRain()
+        {
+            GameObject Rain = GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Environment/WeatherDayNight/");
+            if (Rain != null)
+            {
+                Rain.SetActive(false);
+                MelonLogger.Msg("Removed Rain yay");
+            }
+        }
+
+        public override void OnInitializeMelon() {
+            MelonLogger.Msg("OnApplicationStart");
+        }
+
+        public override void OnLateInitializeMelon() // Runs after OnApplicationStart.
+        {
+            MelonLogger.Msg("OnApplicationLateStart");
+        }
+
+        public override void OnSceneWasLoaded(int buildindex, string sceneName) // Runs when a Scene has Loaded and is passed the Scene's Build Index and Name.
+        {
+            MelonLogger.Msg("OnSceneWasLoaded: " + buildindex.ToString() + " | " + sceneName);
+            RemoveRain();
+        }
+
+        public override void OnSceneWasInitialized(int buildindex, string sceneName) // Runs when a Scene has Initialized and is passed the Scene's Build Index and Name.
+        {
+            MelonLogger.Msg("OnSceneWasInitialized: " + buildindex.ToString() + " | " + sceneName);
+            RemoveRain();
+        }
+
+        public override void OnSceneWasUnloaded(int buildIndex, string sceneName) {
+            MelonLogger.Msg("OnSceneWasUnloaded: " + buildIndex.ToString() + " | " + sceneName);
+        }
+
+        public override void OnUpdate() // Runs once per frame.
+        {
+            //MelonLogger.Msg("OnUpdate");
+        }
+
+        public override void OnFixedUpdate() // Can run multiple times per frame. Mostly used for Physics.
+        {
+            //MelonLogger.Msg("OnFixedUpdate");
+        }
+
+        public override void OnLateUpdate() // Runs once per frame after OnUpdate and OnFixedUpdate have finished.
+        {
+            //MelonLogger.Msg("OnLateUpdate");
+        }
+
+        public override void OnGUI() // Can run multiple times per frame. Mostly used for Unity's IMGUI.
+        {
+            //MelonLogger.Msg("OnGUI");
+        }
+
+        public override void OnApplicationQuit() // Runs when the Game is told to Close.
+        {
+            MelonLogger.Msg("OnApplicationQuit");
+        }
+
+        public override void OnPreferencesSaved() // Runs when Melon Preferences get saved.
+        {
+            MelonLogger.Msg("OnPreferencesSaved");
+        }
+
+        public override void OnPreferencesLoaded() // Runs when Melon Preferences get loaded.
+        {
+            MelonLogger.Msg("OnPreferencesLoaded");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using MelonLoader;
+
+[assembly: AssemblyTitle(RainRemover.BuildInfo.Description)]
+[assembly: AssemblyDescription(RainRemover.BuildInfo.Description)]
+[assembly: AssemblyCompany(RainRemover.BuildInfo.Company)]
+[assembly: AssemblyProduct(RainRemover.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + RainRemover.BuildInfo.Author)]
+[assembly: AssemblyTrademark(RainRemover.BuildInfo.Company)]
+[assembly: AssemblyVersion(RainRemover.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(RainRemover.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(RainRemover.RainRemover), RainRemover.BuildInfo.Name, RainRemover.BuildInfo.Version, RainRemover.BuildInfo.Author, RainRemover.BuildInfo.DownloadLink)]
+[assembly: MelonColor()]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
